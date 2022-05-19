0957fa1c0893431bd5d90ea49d76a8fe02c09275
Aurélien Girault <me@agirault.com>
2022-05-19 18:52:37 +0200

    :sparkles: add the countdown

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,27 @@
 <script>
-import Background from './components/Background.svelte';
+import Countdown from 'svelte-countdown/src/index.js'
 
+import Background from './components/Background.svelte'
 </script>
 
 <main class="flex justify-center align-middle h-full">
 	<Background />
 	<div class="flex flex-col h-full">
+		<h1 class="title absolute text-center mt-4 text-7xl font-indieflower text-white">Bean arrive bientôt !</h1>
 		<div class="bubble relative">
-			<span class="coming absolute text-xl font-mono"></span>
+			<span class="coming absolute text-lg font-indieflower">
+				<Countdown from="2022-07-02 08:30:00" dateFormat="YYYY-MM-DD H:m:s" zone="Europe/Paris" let:remaining>
+					{#if remaining.done === false}
+					J'arrive dans
+					<span>{remaining.weeks} semaines</span>
+					<span>{remaining.days} jours</span>
+					<span>{remaining.hours} heures</span>
+					<span>et {remaining.minutes} minutes !</span>
+					{:else}
+					<h2>Je compte...</h2>
+					{/if}
+			</Countdown>
+			</span>
 		</div>
 		<img src="images/bean.png" class="bean absolute w-52 z-50" alt="Un petit haricot" />
 	</div>
@@ -18,23 +32,48 @@ import Background from './components/Background.svelte';
 @tailwind components;
 @tailwind utilities;
 
+@font-face {
+  font-family: 'IndieFlower';
+  src: url('../public/fonts/IndieFlower-Regular.ttf') format('truetype');
+}
+
+.title {
+	left: 28%;
+	z-index: 1000;
+}
 .bubble {
 	background: url('/images/speech-bubble.png') no-repeat center center / 100% auto;
-	width: 180px;
-	height: 180px;
+	width: 256px;
+	height: 256px;
 	z-index: 1000;
-	top: 50%;
+	top: 44%;
 	right: 110px;
 }
 
 .coming {
-	top: 68px;
+	width: 200px;
+	top: 66px;
 	left: 32px;
 }
 .bean {
 	bottom: 10px;
   animation: rotation 4s infinite ease;
 	z-index: 1000;
+}
+
+@media screen and (max-device-width: 480px) {
+	.title {
+		left: 0;
+	}
+
+	.bubble {
+		top: 32%;
+    right: 58px;
+	}
+
+	.bean {
+		left: 130px;
+	}
 }
 
 @keyframes rotation {
